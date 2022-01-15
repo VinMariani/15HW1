a03be93855b3a95b57483be3687bd209984946f4
VinMariani <lady.mescheryakowa@yandex.ru>
2022-01-15 23:20:13 +0300

    исправления

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 import json
 import sqlite3
 
 app = Flask(__name__)
 
 
-@app.route("/animal/<itemid>")  # страничка вывода одного животного по ID
+@app.route("/animal/id/<int:itemid>")  # страничка вывода одного животного по ID
 def animal_id_page(itemid):
     con = sqlite3.connect('animal.db')
     cur = con.cursor()
@@ -17,26 +17,27 @@ def animal_id_page(itemid):
     animals = cur.fetchall()
 
     result_animals = {
-        "Result 1": animals[0],
-        "Result 2": animals[1],
-        "Result 3": animals[2],
-        "Result 4": animals[3],
-        "Result 5": animals[4]
+        "Result": animals[0],
+        #"Result 2": animals[1],
+        #"Result 3": animals[2],
+        #"Result 4": animals[3],
+        #"Result 5": animals[4]
     }
 
     con.close()
-    return result_animals
-    # return jsonify(result_animals)
+    #return result_animals
+    return jsonify(result_animals)
 
 
-@app.route("/animal/<name>")  # страничка вывода одного животного по имени
+@app.route("/animal/name/<name>")  # страничка вывода одного животного по имени
 def animal_name_page(name):
     con = sqlite3.connect('animal.db')
     cur = con.cursor()
+    name = request.args.get("name")
     query = f"""select * 
                 from unique_animal
                 left join outcomes on outcomes.animal_id=outcomes.animal_id
-                where unique_animal.name = {name}"""
+                where unique_animal.name = '{name}'"""
     cur.execute(query)
     animals = cur.fetchall()
 
